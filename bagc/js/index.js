c5a1fb32fb202bed77e68c1910fab4a2a845ac86
jiazhuang89 <jfx20105706@163.com>
2017-08-02 11:01:55 +0800

    修改js

--- bagc/js/index.js
+++ bagc/js/index.js
@@ -2,17 +2,3 @@
 $(document).ready(function(){
    window.scrollTo(0,$('.home-swiper').height()-205);
   });
-  // 留言提交
-$(document).ready(function(){
-  $('#Fform').ajaxForm({
-      beforeSubmit: function () {
-          return true;
-      },
-      dataType: 'json',
-      success: function (data) {
-          alert(data['message'] || '失败');
-      },
-      error: function() {
-      }
-  });
-});
